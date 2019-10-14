62e05e0d4a89b479b64a88ba0bee67946e5cbd2b
蒋涛 <jiang7210t@gmail.com>
2019-10-14 12:57:57 +0800

    KNN与决策树

---
+++ 1.KNN/knn.py
@@ -0,0 +1,171 @@
+from numpy import *
+from numpy import *
+import operator
+from os import listdir
+import matplotlib.pyplot as plt
+
+def createDataSet():
+
+    """
+    创建数据集和标签
+     调用方式
+     import kNN
+     group, labels = kNN.createDataSet()
+     """
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['爱情片', '爱情片', '动作片', '动作片']
+    return group, labels
+
+#Knn分类算法
+def classify0(inX, dataSet, labels, k):
+    # 1. 距离计算
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet      # tile生成和训练样本对应的矩阵，并与训练样本求差
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)      # 将矩阵的每一行相加
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()  #  argsort() 是将x中的元素从小到大排列，提取其对应的index（索引），然后输出到y。
+    # 2. 选择距离最小的k个点
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    maxClassCount = max(classCount, key=classCount.get)
+    return maxClassCount
+
+def test1():
+    """
+    第一个例子演示
+    """
+    group, labels = createDataSet()
+    print("电影数据集为:",str(group))
+    print("电影标签为:",str(labels))
+    print("预测电影类型为:",classify0([0.1, 0.1], group, labels, 3))
+
+
+def file2matrix(filename):
+    """
+    导入训练数据
+    :param filename: 数据文件路径
+    :return: 数据矩阵returnMat和对应的类别classLabelVector
+    """
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())
+    # 生成对应的空矩阵
+    # 例如：zeros(2，3)就是生成一个 2*3的矩阵，各个位置上全是 0
+    returnMat = zeros((numberOfLines, 3))  # prepare matrix to return
+    classLabelVector = []  # prepare labels return
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        # str.strip([chars]) --返回移除字符串头尾指定的字符生成的新字符串
+        line = line.strip()
+        # 以 '\t' 切割字符串
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]   #：左边表示对矩阵行的操作，右边表示对列的操作
+        # 添加每列的类别数据到最后，就是 label 标签数据
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    # print(returnMat)
+    # 返回数据矩阵returnMat和对应的类别classLabelVector
+    return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+    """
+    归一化特征值，消除属性之间量级不同导致的影响
+    :param dataSet: 数据集
+    :return: 归一化后的数据集normDataSet,ranges和minVals即最小值与范围，并没有用到
+    归一化公式：
+        Y = (X-Xmin)/(Xmax-Xmin)
+        其中的 min 和 max 分别是数据集中的最小特征值和最大特征值。该函数可以自动将数字特征值转化为0到1的区间。
+    """
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    # 极差
+    ranges = maxVals - minVals
+    normDataSet = (dataSet - minVals) / ranges
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    # 设置测试数据的的一个比例（训练数据集比例=1-hoRatio）
+    hoRatio = 0.1  # 测试范围,一部分测试一部分作为样本
+    # 从文件中加载数据
+    datingDataMat, datingLabels = file2matrix('data/datingTestSet2.txt')
+
+    #分析数据，画散点图
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(datingDataMat[:, 0], datingDataMat[:, 1], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
+    plt.show()
+
+    # 归一化数据
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    # m 表示数据的行数，即矩阵的第一维
+    m = normMat.shape[0]
+    # 设置测试的样本数量， numTestVecs:m表示训练样本的数量
+    numTestVecs = int(m * hoRatio)
+    print('测试样本数量=', numTestVecs)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        # 对数据测试
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print("分类器的结果是: %d, 正确答案是: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print("错误率是: %f" % (errorCount / float(numTestVecs)))
+    print('错误数量:',errorCount,)
+
+
+def img2vector(filename):
+    """
+    将图像数据转换为向量
+    :param filename: 图片文件 因为我们的输入数据的图片格式是 32 * 32的
+    :return: 一维矩阵
+    该函数将图像转换为向量：该函数创建 1 * 1024 的NumPy数组，然后打开给定的文件，
+    循环读出文件的前32行，并将每行的头32个字符值存储在NumPy数组中，最后返回数组。
+    """
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    # 1. 导入数据
+    hwLabels = []
+    trainingFileList = listdir('data/trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    # hwLabels存储0～9对应的index位置， trainingMat存放的每个位置对应的图片向量
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        # 将 32*32的矩阵->1*1024的矩阵
+        trainingMat[i, :] = img2vector('data/trainingDigits/%s' % fileNameStr)
+
+    # 2. 导入测试数据
+    testFileList = listdir('data/testDigits')  # iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('data/testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("分类识别数是: %d, 实际数字是: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print("\n错误的个数是: %d" % errorCount)
+    print("\n手写识别错误率是: %f" % (errorCount / float(mTest)))
+
+
+if __name__ == '__main__':
+    test1()
+    datingClassTest()
+
+
+    handwritingClassTest()
---
+++ 2.DecisonTree/DecisionTree.py
@@ -0,0 +1,221 @@
+from math import log
+import decisionTreePlot as dtPlot
+from collections import Counter
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+              [1, 1, 'yes'],
+              [1, 0, 'no'],
+              [0, 1, 'no'],
+              [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+def calcShannonEnt(dataSet):
+    """calcShannonEnt(calculate Shannon entropy 计算给定数据集的香农熵)
+    Args:
+        dataSet 数据集
+    Returns:
+        返回 每一组feature下的某个分类下，香农熵的信息期望
+    """
+    # 统计标签出现的次数
+    label_count = Counter(data[-1] for data in dataSet)
+    # 计算概率
+    probs = [ p[1] / len(dataSet)for p in label_count.items()]
+    # 计算香农熵
+    shannonEnt = sum([-p * log(p, 2) for p in probs])
+    return shannonEnt
+
+def splitDataSet(dataSet, axis, value):
+    """splitDataSet(通过遍历dataSet数据集，求出index对应的colnum列的值为value的行)
+        就是依据index列进行分类，如果index列的数据等于 value的时候，就要将 index 划分到我们创建的新的数据集中
+    Args:
+        dataSet 数据集                 待划分的数据集
+        index 表示每一行的index列        划分数据集的特征
+        value 表示index列对应的value值   需要返回的特征的值。
+    Returns:
+        index列为value的数据集【该数据集需要排除index列】
+    """
+    retDataSet = [data for data in dataSet for i, v in enumerate(data) if i == axis and v == value]
+    return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    """chooseBestFeatureToSplit(选择最好的特征)
+    Args:
+        dataSet 数据集
+    Returns:
+        bestFeature 最优的特征列的索引
+    """
+    # 计算初始香农熵
+    base_entropy = calcShannonEnt(dataSet)
+    best_info_gain = 0
+    best_feature = -1
+    # 遍历每一个特征
+    for i in range(len(dataSet[0]) - 1):
+        # 对当前特征进行统计
+        feature_count = Counter([data[i] for data in dataSet])
+        # 计算各列的各value值被分割数据集后的香农熵
+        new_entropy = sum(feature[1] / float(len(dataSet)) * calcShannonEnt(splitDataSet(dataSet, i, feature[0])) \
+                       for feature in feature_count.items())
+        # 更新值
+        info_gain = base_entropy - new_entropy
+        print('第{0} 列的信息增益是:{1:.3f}'.format(i, info_gain))
+        if  info_gain > best_info_gain:
+            best_info_gain = info_gain
+            best_feature = i
+    print("最好的特征列是:",best_feature)
+    return best_feature
+
+def majorityCnt(classList):
+    """majorityCnt(选择出现次数最多的一个结果)
+        Args:
+            classList label列的集合
+        Returns:
+            bestFeature 最优的特征列
+        """
+    major_label = Counter(classList).most_common(1)[0]
+    return major_label
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    # 如果数据集的最后一列的第一个值出现的次数=整个集合的数量，也就说只有一个类别，就只直接返回结果就行
+    # 第一个停止条件：所有的类标签完全相同，则直接返回该类标签。
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    # 如果数据集只有1列，那么最初出现label次数最多的一类，作为结果
+    # 第二个停止条件：使用完了所有特征，仍然不能将数据集划分成仅包含唯一类别的分组。
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    # 选择最优的列，得到最优列对应的label的索引
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    # 获取label的名称
+    bestFeatLabel = labels[bestFeat]
+    labels.append(bestFeatLabel)
+    # 初始化myTree
+    myTree = {bestFeatLabel: {}}
+    # 注：labels列表是可变对象，在PYTHON函数中作为参数时传址引用，能够被全局修改
+    # 所以这行代码导致函数外的同名变量被删除了元素，造成例句无法执行，提示'no surfacing' is not in list]
+    del(labels[bestFeat])
+    # 取出最优列的所有value值，然后对它的分支做递归建树
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        # 求出剩余的标签label
+        subLabels = labels[:]
+        # 遍历当前选择特征包含的所有属性值，在每个数据集划分上递归调用函数createTree()
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+        # print 'myTree', value, myTree
+    return myTree
+
+
+def classify(inputTree, featLabels, testVec):
+    """classify(给输入的节点，进行分类)
+    Args:
+        inputTree  决策树模型
+        featLabels Feature标签对应的名称
+        testVec    测试输入的数据
+    Returns:
+        classLabel 分类的结果值，需要映射label才能知道名称
+    """
+    # 获取tree的根节点对于的key值,书上这里会报错,必须加list
+    firstStr = list(inputTree.keys())[0]
+    # 通过key得到根节点对应的value
+    secondDict = inputTree[firstStr]
+    # 判断根节点名称获取根节点在label中的先后顺序，这样就知道输入的testVec怎么开始对照树来做分类
+    featIndex = featLabels.index(firstStr)
+    # 测试数据，找到根节点对应的label位置，也就知道从输入的数据的第几位来开始分类
+    key = testVec[featIndex]
+    valueOfFeat = secondDict[key]
+    print('+++', firstStr, 'xxx', secondDict, '---', key, '>>>', valueOfFeat)
+    # 判断分枝是否结束: 判断valueOfFeat是否是dict类型
+    if isinstance(valueOfFeat, dict):
+        classLabel = classify(valueOfFeat, featLabels, testVec)
+    else:
+        classLabel = valueOfFeat
+    return classLabel
+
+def storeTree(inputTree, filename):
+    import pickle
+    # # -------------- 第一种方法 start --------------
+    # fw = open(filename, 'wb')
+    # pickle.dump(inputTree, fw)
+    # fw.close()
+    # # -------------- 第一种方法 end --------------
+
+    # -------------- 第二种方法 start --------------
+    with open(filename, 'wb') as fw:
+        pickle.dump(inputTree, fw)
+
+    # -------------- 第二种方法 start --------------
+
+def grabTree(filename):
+    import pickle
+    fr = open(filename,'rb')
+    return pickle.load(fr)
+
+def fishTest():
+    import copy
+    # 1.创建数据和结果标签
+    myDat,labels = createDataSet()
+    myTree = createTree(myDat, copy.deepcopy(labels))
+    print("决策树是:",myTree)
+    #2.分类树的存取
+    storeTree(myTree,'dt.txt')
+    #3.[1, 1]表示要取的分支上的节点位置，对应的结果值
+    print(classify(myTree, labels, [1, 1]))
+    #4.获得树的高度
+    print("树的高度是:",get_tree_height(myTree))
+    #
+    # 4.画图可视化展现fishTest
+    dtPlot.createPlot(myTree)
+
+def get_tree_height(tree):
+    """
+     Desc:
+        递归获得决策树的高度
+    Args:
+        tree
+    Returns:
+        树高
+    """
+
+    if not isinstance(tree, dict):
+        return 1
+
+    child_trees = list(tree.values())[0].values()
+
+    # 遍历子树, 获得子树的最大高度
+    max_height = 0
+    for child_tree in child_trees:
+        child_tree_height = get_tree_height(child_tree)
+
+        if child_tree_height > max_height:
+            max_height = child_tree_height
+
+    return max_height + 1
+
+def ContactLensesTest():
+    """
+    Desc:
+        预测隐形眼镜的测试代码
+    Args:
+        none
+    Returns:
+        none
+    """
+    # 加载隐形眼镜相关的 文本文件 数据
+    fr = open('data/lenses.txt')
+    # 解析数据，获得 features 数据
+    lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+    # 得到数据的对应的 Labels
+    lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+    # 使用上面的创建决策树的代码，构造预测隐形眼镜的决策树
+    lensesTree = createTree(lenses, lensesLabels)
+    print(lensesTree)
+    # 画图可视化展现
+    dtPlot.createPlot(lensesTree)
+
+if __name__ == "__main__":
+    print("取出的树是:",grabTree('dt.txt'))
+    fishTest()
+    ContactLensesTest()
